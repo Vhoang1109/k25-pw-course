7fa1c2fb60d6ef8e180a43e9bdfdf3ea51c5217d
Hoang <vhoang1109@gmail.com>
2026-04-15 15:02:18 +0700

    add all files js buoi2

---
+++ tests/example/07-loops.js
@@ -0,0 +1,28 @@
+/* 
+for (<khoi tao>; <dieu kien dung>; <dieu kien tang>) {
+    // logic code
+}
+    */
+//without for
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+console.log("-----");
+
+// cach hoc code
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+/*
+i = 1 
+kiem tra i co nho hon = 10 hay khong
+dung thi chay logic code
+chay vao i++
+...
+i = 11
+kiem tra i co nho hon = 10 hay khong
+sai thoat khoi vong lap
+*/
